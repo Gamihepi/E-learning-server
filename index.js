6fefb8090082aac721b6b637544d3a31db60403a
Hepi <121499922+Gamihepi@users.noreply.github.com>
2025-04-21 11:07:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,10 +16,7 @@ export const instance = new Razorpay({
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:5173', // your frontend URL
-    credentials: true
-  }));
+app.use(cors());
 
 const port = process.env.PORT ;
 
